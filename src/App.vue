d93d329fc5806ee02943130905017b324fc3af9d
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-12-16 19:54:33 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestPage from "./components/TestPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TestPage,
+  },
+};
 </script>
 
 <style>
